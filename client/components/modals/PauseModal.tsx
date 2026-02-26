aa62002fa3ac6e962aabf0a624f0286fabddb019
Avanish Singh <singhavanish2003@gmail.com>
2026-02-26 14:31:11 +0530

    suffix Input component

--- client/components/modals/PauseModal.tsx
+++ client/components/modals/PauseModal.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import React from 'react';
 import { AlertTriangle } from 'lucide-react';
 import { Modal } from '@/components/common/Modal';
 import { Button } from '@/components/common/Button';
